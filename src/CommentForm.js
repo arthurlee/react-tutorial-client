471f5a5f00711158c2f483f4d92c658e6adc415f
arthurlee <boyal2008@gmail.com>
2016-09-07 13:49:03 +0800

    empty add form

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -3,11 +3,53 @@
  */
 
 var CommentForm = React.createClass({
+	getInitialState() {
+		return {
+			author: '',
+			text: ''
+		};
+	},
+
+	handleAuthorChange: function (e) {
+		this.setState({author: e.target.value});
+	},
+
+	handleTextChange: function (e) {
+		this.setState({text: e.target.value});
+	},
+
+	handleSubmit: function (e) {
+		e.preventDefault();
+
+		var author = this.state.author.trim();
+		var text = this.state.text.trim();
+		if (!author || !text) {
+			return;
+		}
+
+		this.setState({
+			author: '',
+			text: 'ok'
+		});
+	},
+
 	render: function() {
 		return (
-			<div className="commentForm">
-				Hello, world! I am a CommentForm.
-			</div>
+			<form className="commentForm">
+				<input
+					type="text"
+					placeholder="Your name"
+					value={this.state.author}
+					onChange={this.handleAuthorChange}
+				/>
+				<input
+					type="text"
+					placeholder="Say something..."
+					value={this.state.text}
+					onChange={this.handleTextChange}
+				/>
+				<input type="submit" value="Post" />
+			</form>
 		);
 	}
 });
